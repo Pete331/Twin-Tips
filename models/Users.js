6cd3e2b8add21f2d5a0ca95bc34d363ac5484aa2
Pete Brennan <pweda64@hotmail.com>
2020-07-20 19:23:25 +0800

    added admin to schema

--- models/Users.js
+++ models/Users.js
@@ -34,6 +34,10 @@ const UserSchema = new Schema(
       type: Number,
       required: true,
     },
+    admin: {
+        type: Boolean,
+        default: false,
+      },
   },
   {
     timestamps: true,
