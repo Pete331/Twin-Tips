bc4ccf26aa41fd04e79b5de2bd6e8938b2152693
Peter Brennan <pweda64@hotmail.com>
2020-08-05 14:21:24 +0800

    Update nodeMailer.js

--- utils/nodeMailer.js
+++ utils/nodeMailer.js
@@ -267,10 +267,8 @@ const sendMail = async (email, token, fName) => {
       port: 587,
       secure: false, // true for 465, false for other ports(587)
       auth: {
-                user: twin-tips@outlook.com,
-        pass: TwinTips331,
-        // user: process.env.EMAIL_USER,
-        // pass: process.env.EMAIL_PASSWORD,
+         user: process.env.EMAIL_USER,
+         pass: process.env.EMAIL_PASSWORD,
       },
       tls: {
         ciphers:'SSLv3',
